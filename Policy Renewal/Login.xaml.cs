2121d1754e9836b1ae5ff4af524b199678e2d665
I523349 <I523349@WDFN34719926A>
2020-01-30 21:01:38 +0100

    changing the layout of main page and adding Login page

---
+++ Policy Renewal/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Policy_Renewal
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPasswordBox.Password.ToString() == "0710" )
+            {
+                var win = new MainWindow();
+                win.Owner = this;
+                win.Height = 700.967;
+                win.Width = 1160.154;
+                win.Show();
+                Visibility = Visibility.Collapsed;
+            }
+
+            else
+            {
+                MessageBox.Show("Wrong PIN", "Error");
+            }
+        }
+    }
+}
